5853395da0cc4c08a1905b9652b42b290ba545ed
Vinícius Zanotti de Oliveira <vinizanotti@gmail.com>
2025-08-02 00:19:40 -0300

    Base components BestSellers and CardProduct

---
+++ fakestore-interface/src/components/BestSellers/index.jsx
@@ -0,0 +1,10 @@
+
+
+export const BestSellers = () => {
+  return (
+    <div className="best-sellers">
+      <h2>Best Sellers</h2>
+      {/* Add your best sellers content here */}
+    </div>
+  );
+};
